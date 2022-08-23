ec24bdb8e2593fe0b84e7d91219f43e137a1e1a6
Kant <88287313+Kant2510@users.noreply.github.com>
2022-08-23 19:45:39 +0700

    Update get_raw_data.py

--- main/get_raw_data.py
+++ main/get_raw_data.py
@@ -1,8 +1,7 @@
 import requests
 
-file = open("Output.txt", "a", encoding="utf-8")
+file = open("raw.txt", "a", encoding="utf-8")
 url = "https://m.diemthi.tuyensinh247.com/tracuu/ajaxDiemthiTHPT"
-res = requests.post(url, data = load)
 begin = 35000001 #first candidate number
 end = 35013381 #last candidate number
 for sbd in range(begin, end + 1):
